11fbab4628e2a93c19cf9e12f9efcf81526726b5
uota <uota1921@gmail.com>
2025-08-26 00:59:20 +0900

    ContactForm7に使用するCSS・JSの読み込み先を修正

--- app/functions/plugins.php
+++ app/functions/plugins.php
@@ -12,10 +12,10 @@ if ( !defined( 'ABSPATH' ) ) exit;
 function enqueue_custom_assets_for_specific_page() {
     if (is_page('contact')) { // 固定ページのスラッグ指定
         // CSSファイルの読み込み
-        wp_enqueue_style('custom-page-style', get_template_directory_uri() . '/public/plugins/p_cf7/style.css', array(), '1.0.0');
+        wp_enqueue_style('custom-page-style', get_template_directory_uri() . '/app/plugins/p_cf7/style.css', array(), '1.0.0');
         
         // JavaScriptファイルの読み込み
-        wp_enqueue_script('custom-page-script', get_template_directory_uri() . '/public/plugins/p_cf7/form.js', array('jquery'), '1.0.0', true);
+        wp_enqueue_script('custom-page-script', get_template_directory_uri() . '/app/plugins/p_cf7/form.js', array('jquery'), '1.0.0', true);
         
         // Contact Form 7のデフォルトバリデーションメッセージを非表示にするCSS
         wp_add_inline_style('custom-page-style', '
