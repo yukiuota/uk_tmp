66ee73b830e6e345839362adc89809d99317b218
uota <uota1921@gmail.com>
2025-09-11 23:54:10 +0900

    サンプルをコメントアウト

--- app/controllers/news_controller.php
+++ app/controllers/news_controller.php
@@ -1,8 +1,8 @@
 <?php
 if ( !defined( 'ABSPATH' ) ) exit;
 
-// カスタムフィールド取得
-$fields = get_fields();
-$field1 = $fields['text01'];
-$text02 = $fields['text02'];
+// カスタムフィールド取得sample
+// $fields = get_fields();
+// $field1 = $fields['text01'];
+// $text02 = $fields['text02'];
 ?>
