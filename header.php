65d0bded0b159d2fed48df5e2b5dc9160f053e7b
uota <uota1921@gmail.com>
2025-09-13 01:37:56 +0900

    OGP設定を修正

--- header.php
+++ header.php
@@ -1,13 +1,18 @@
 <?php if ( ! defined( 'ABSPATH' ) ) exit; ?>
 <!doctype html>
 <html <?php language_attributes(); ?>>
+<?php if ( is_home() || is_front_page() ) : ?>
 
 <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
-    <?php get_template_part( 'public/include/tags/head_top' ); // head_tag ?>
-    <meta charset="<?php bloginfo( 'charset' ); ?>">
-    <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
-    <?php wp_head(); ?>
-</head>
+    <?php else : ?>
+
+    <head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# article: http://ogp.me/ns/article#">
+        <?php endif; ?>
+        <?php get_template_part( 'public/include/tags/head_top' ); // head_tag ?>
+        <meta charset="<?php bloginfo( 'charset' ); ?>">
+        <meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0" />
+        <?php wp_head(); ?>
+    </head>
 
 <body <?php body_class(); ?>>
     <?php wp_body_open(); ?>
